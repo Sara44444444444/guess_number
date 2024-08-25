62f223cdd36fee7ef3eb49e786fc52da666081a4
sara hashemi <hashemi.sara277@gmail.com>
2024-08-25 11:15:32 +0200

    guess number gui creat

--- guess_number_cmd.py
+++ guess_number_cmd.py
@@ -1,14 +1,17 @@
 #Esta programa tiene que continuar hasta que sea correcto
 import random
 
-number1 = random.randint(1, 3)
+guessnumber = random.randint(1, 50)
 while True:
     number = int(input("number:"))
-    if number==number1:
+    if number==guessnumber:
         print("Is correct,very good")
         break
     else:
-        print("Is false, try again")
+        if number>guessnumber:
+            print("number is less")
+        if number<guessnumber:
+            print("number is more")
 
 
 
---
+++ guess_number_gui.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+m = Tk()
+w = Label(m,text="gui-guess-number")
+w.pack()
+dokme=Button(m,text="check number")
+dokme.pack()
+
+
+m.mainloop()
