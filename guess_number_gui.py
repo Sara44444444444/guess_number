bf0166927dc7a5c43254541fc78ef06fbf6e46f0
sara hashemi <hashemi.sara277@gmail.com>
2024-08-29 22:45:48 +0200

    Set the background color

--- guess_number_gui.py
+++ guess_number_gui.py
@@ -1,24 +1,29 @@
 from tkinter import *
 
 root = Tk()
-root.geometry("1000x1000")  # Set the window size
+root.geometry("1920x1080")  # Set the window size
+root.configure(bg="light blue")  # Set the background color
+
 
 # creating a Label Widget
-myLabel1 = Label(root, text="Guess My Number", font=("Helvetica", 16, "bold"),fg=("blue"))
-myLabel2 = Label(root, text="Guess a random number", font=("Helvetica",15,"bold"),fg=("blue"))
+myLabel1 = Label(root, text="Guess My Number", font=("Helvetica", 16, "bold"), fg="blue")
+myLabel2 = Label(root, text="Guess a random number", font=("Helvetica", 15, "bold"), fg="blue")
 
 # Shoving it onto the screen in the center
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
 
-# Creating Buttons
-myButton1 = Button(root, text="1")
-myButton2 = Button(root, text="100")
+# Creating Entry Widgets for user input
+entry1 = Entry(root, width=10)
+entry2 = Entry(root, width=10)
 
-# Placing the buttons side by side
-myButton1.grid(row=2, column=0)
-myButton2.grid(row=2, column=1)
+# Creating a Button
+button = Button(root, text="New target", fg="blue", bg="light blue")
 
+# Placing the Entry widgets and Button in the grid
+entry1.grid(row=2, column=0)
+entry2.grid(row=2, column=1)
+button.grid(row=2, column=2)
 
-
+# Running the main loop
 root.mainloop()
