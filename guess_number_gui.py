9bfd03f8a5ba2ad27ce5eaaa52006b63dab345e6
sara hashemi <hashemi.sara277@gmail.com>
2024-08-29 22:13:18 +0200

    creating a Label Widget

--- guess_number_gui.py
+++ guess_number_gui.py
@@ -1,11 +1,16 @@
 import tkinter
 from tkinter import *
 
-m = Tk()
-w = Label(m,text="gui-guess-number")
-w.pack()
-dokme=Button(m,text="check number")
-dokme.pack()
+root = Tk()
+root.geometry("1000x1000")  # Set the window size
+
+# creating a Label Widget
+myLabel1 = Label(root, text="Guess My Number", font=("Helvetica", 16, "bold"),fg=("blue"))
+myLabel2 = Label(root, text="Guess a random number", font=("Helvetica",15,"bold"),fg=("blue"))
+# Shoving it onto the screen in the center
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=0)
 
 
-m.mainloop()
+root.mainloop()
