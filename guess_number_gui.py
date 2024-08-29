b8362c85354f264e4e6c2839c599f086d0a60e17
sara hashemi <hashemi.sara277@gmail.com>
2024-08-29 22:23:56 +0200

    Creating Buttons

--- guess_number_gui.py
+++ guess_number_gui.py
@@ -1,4 +1,3 @@
-import tkinter
 from tkinter import *
 
 root = Tk()
@@ -7,10 +6,19 @@ root.geometry("1000x1000")  # Set the window size
 # creating a Label Widget
 myLabel1 = Label(root, text="Guess My Number", font=("Helvetica", 16, "bold"),fg=("blue"))
 myLabel2 = Label(root, text="Guess a random number", font=("Helvetica",15,"bold"),fg=("blue"))
-# Shoving it onto the screen in the center
 
+# Shoving it onto the screen in the center
 myLabel1.grid(row=0, column=0)
 myLabel2.grid(row=1, column=0)
+
+# Creating Buttons
+myButton1 = Button(root, text="1")
+myButton2 = Button(root, text="100")
+
+# Placing the buttons side by side
+myButton1.grid(row=2, column=0)
+myButton2.grid(row=2, column=1)
+
 
 
 root.mainloop()
