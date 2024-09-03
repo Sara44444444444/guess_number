0c3901fa8e3c4a7f7894065306570d00f8ec62ca
sara hashemi <hashemi.sara277@gmail.com>
2024-09-03 13:37:03 +0200

    code is ok

--- guess_number_gui.py
+++ guess_number_gui.py
@@ -1,35 +1,67 @@
 from tkinter import *
+import random
+
+target_number = None
+
+
+def Guess():
+    num = int(entryGuessNumber.get())
+    global target_number
+
+    if target_number == None:
+        resultLabel.config(text="please first click the button New target ")
+        return
+    if num > target_number:
+        resultLabel.config(text="My number is less")
+    elif num < target_number:
+        resultLabel.config(text="My number is more")
+    else:
+        resultLabel.config(text="well Done!")
+
+
+def set_new_target():
+    global target_number
+    lownumber = int(lowNumberEntry.get())
+    highnumber = int(highNumberEntry.get())
+    target_number = random.randint(lownumber, highnumber)
+    print(target_number)  # adad tasadofi hastesh
+
 
 root = Tk()
 root.geometry("1920x1080")  # Set the window size
 root.configure(bg="light blue")  # Set the background color
 
-# creating a Label Widget
-myLabel1 = Label(root, text="Guess My Number", font=("Helvetica", 16, "bold"), fg="blue")
-myLabel2 = Label(root, text="Guess a random number", font=("Helvetica", 15, "bold"), fg="blue")
+# Creating a Frame with a border
+border_frame = Frame(root, borderwidth=5, relief="ridge", bg="light blue")
+border_frame.pack(fill="both", expand=True, padx=10, pady=10)
 
-# Shoving it onto the screen in the center
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+GuessMyNumber = Label(border_frame, text="Guess My Number", font=("arial", 16, "bold"), bg="light blue")
+GuessMyNumber.pack()
+myLabel2 = Label(border_frame, text="Guess a random number", bg="light blue")
+myLabel2.pack()
 
-# Creating Entry Widgets for user input
-entry1 = Entry(root, width=10)
-entry2 = Entry(root, width=10)
+frame = Frame(border_frame, bg="light blue")
+frame.pack(pady=10)
 
-# Creating a Button
-button = Button(root, text="New target", fg="blue", bg="light blue")
+lowNumberEntry = Entry(frame, width=10)
+lowNumberEntry.pack(side=LEFT, padx=10)
+lowNumberEntry.insert(0, str(1))
+highNumberEntry = Entry(frame, width=10)
+highNumberEntry.pack(side=LEFT, padx=10)
+highNumberEntry.insert(0, str(100))
+buttonSetNewTarget = Button(frame, text="New Target", bg="light blue", fg="dark blue", command=set_new_target)
+buttonSetNewTarget.pack(side=LEFT, padx=10)
 
-# Placing the Entry widgets and Button in the grid
-entry1.grid(row=2, column=0)
-entry2.grid(row=2, column=1)
-button.grid(row=2, column=2)
+resultLabel = Label(border_frame, text="Please guess a number, enter it and press guess:", bg="light yellow")
+resultLabel.pack()
 
-# creating myLabel3
-myLabel3 = Label(root, text="Please guess a number, enter it, and press guess:")
-myLabel3.grid(row=3, column=0)
-# creating a Button for myLabel3
-button1 = Button(root, text="Guess")
-button1.grid(row=3, column=1)
+frame1 = Frame(border_frame, bg="light blue")
+frame1.pack(pady=12)
+
+entryGuessNumber = Entry(frame1, width=10)
+entryGuessNumber.pack(side=LEFT, padx=10)
+buttonGuess = Button(frame1, text="Guess", command=Guess)
+buttonGuess.pack(side=LEFT, padx=10)
 
 # Running the main loop
 root.mainloop()
